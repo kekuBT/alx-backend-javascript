e675d0704c48e87cb67b3a119358d1a9352c53be
Keku Belina <kekubt@gmail.com>
2023-01-04 12:07:34 -0500

    linting

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,34 +1,34 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
-    [propName: string]: any,
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [propName: string]: any,
 }
 
 interface Directors extends Teacher {
-    numberOfReports: number,
+  numberOfReports: number,
 }
 
 const printTeacher = (firstName: string, lastName: string): string => `${firstName.charAt(0)}. ${lastName}`;
 
 class StudentClass {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 
-    constructor(firstName: string, lastName: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-    workOnHomework = (): string => 'Currently working';
+  workOnHomework = (): string => 'Currently working';
 
-    displayName = (): string => this.firstName;
+  displayName = (): string => this.firstName;
 }
 
 interface StudentConstructor {
-    (firstName: string, lastName: string): classInterface;
+  (firstName: string, lastName: string): classInterface;
 }
 
 const student = new StudentClass("Paul", "jerry");
