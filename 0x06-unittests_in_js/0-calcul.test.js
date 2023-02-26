628d752ba1edc8a2aba35bd667cdd28e0836bea2
Keku Belina <kekubt@gmail.com>
2023-02-26 05:55:16 -0500

    edit

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,12 +8,13 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, -1), 0);
     assert.strictEqual(calculateNumber(1, -3), -2);
-  });
+  })
+});
 
-  it('should round floats', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(0.1, 0), 0);
-    assert.strictEqual(calculateNumber(1.4, -4.5), -3);
-  });
+it('should round floats', () => {
+  assert.strictEqual(calculateNumber(1, 3.7), 5);
+  assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  assert.strictEqual(calculateNumber(0.1, 0), 0);
+  assert.strictEqual(calculateNumber(1.4, -4.5), -3);
+});
